2bda0ac3e3b9277d563510833ae02d981bb6dd9f
Anna Popova <anna.popova@drivedominion.com>
2015-02-04 15:59:28 -0500

    Ex.Activity 23-26
    Adding views:
    
    vw_Customers
    vw_Vehicles

---
+++ SSDT_Team_Exercise_1/Inventory/vw_Vehicles.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [Inventory].[vw_Vehicle]
+	AS SELECT [m].[Name] AS [Make],[m2].[Name] AS [Model],[c].[Name] AS [Color],[Year],[Price]
+	FROM [Inventory].[Vehicle] AS v
+	INNER JOIN [Inventory].[Color] AS c ON [c].[Id] = [v].[ColorId]
+	INNER JOIN [Inventory].[Make] AS m ON [m].[Id] = [v].[MakeId]
+	INNER JOIN [Inventory].[Model] AS m2 ON [m2].[Id] = [v].[ModelId]
---
+++ SSDT_Team_Exercise_1/Person/vw_Customers.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [Person].[vw_Customers]
+	AS SELECT  [FName],[LName],[BirthDate]
+	FROM [Person].[Customer]
+	INNER JOIN [Person].[Person] AS p ON [p].[Id] = [Customer].[PersonId]
