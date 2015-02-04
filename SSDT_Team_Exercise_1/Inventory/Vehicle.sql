fb5f47bc2917b3cb90a820773082f7353e58bd8f
Anna Popova <anna.popova@drivedominion.com>
2015-02-04 13:50:50 -0500

    Ex. Activity 12-18
    Adding tables :
    
    Inventory.Model
    Inventory.Make
    Inventory.Color
    Inventory.Vehicle

---
+++ SSDT_Team_Exercise_1/Inventory/Vehicle.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Inventory].[Vehicle]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
+    [MakeId] UNIQUEIDENTIFIER NOT NULL, 
+    [ModelId] UNIQUEIDENTIFIER NOT NULL, 
+    [Year] SMALLINT NOT NULL, 
+    [ColorId] UNIQUEIDENTIFIER NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    CONSTRAINT [FK_Vehicle_Make]  FOREIGN KEY ([MakeId])  REFERENCES Inventory.[Make]([Id]), 
+    CONSTRAINT [FK_Vehicle_Model] FOREIGN KEY ([ModelId]) REFERENCES [Inventory].[Model]([Id]), 
+    CONSTRAINT [FK_Vehicle_Color] FOREIGN KEY ([ColorId]) REFERENCES Inventory.[Color]([Id])
+)
