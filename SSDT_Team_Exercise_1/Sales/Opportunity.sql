e5303fbbfc4fe6c233bca44a362dc310a2489752
Anna Popova <anna.popova@drivedominion.com>
2015-02-04 14:22:35 -0500

    Ex.Activity  18-22
    Adding tables:
    
    Sales.Opportunity
    Sales.OpportunityType

---
+++ SSDT_Team_Exercise_1/Sales/Opportunity.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Sales].[Opportunity]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
+    [CustomerId] UNIQUEIDENTIFIER NOT NULL, 
+    [OpportunityTypeId] UNIQUEIDENTIFIER NOT NULL, 
+    [CreatedDate] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
+    [ModifiedDate] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
+    CONSTRAINT [FK_Opportunity_OpportunityType] FOREIGN KEY ([OpportunityTypeId]) REFERENCES [Sales].[OpportunityType]([Id]), 
+    CONSTRAINT [FK_Opportunity_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [Person].[Customer]([id])
+)
