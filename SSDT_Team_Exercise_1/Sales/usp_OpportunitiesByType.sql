f66f197c59840fabdb78efcb9a072fce1e780bf7
Anna Popova <anna.popova@drivedominion.com>
2015-02-09 10:18:17 -0500

    Ex.Activity 23-27
    Adding views:
    Inventory.vw_Vehicles
    Person.vw_Customers
    
    Adding stored procedure:
    Sales.usp_OpportunitiesByType

---
+++ SSDT_Team_Exercise_1/Sales/usp_OpportunitiesByType.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [Sales].[usp_OpportunitiesByType]
+	@Type NVARCHAR(50) = ''
+AS
+	SELECT ot.[Name],p.[FName],p.[LName]
+	FROM [Sales].[Opportunity] AS o
+	INNER JOIN [Sales].[OpportunityType] AS ot ON [ot].[Id] = [o].[OpportunityTypeId]
+	INNER JOIN [Person].[Customer] AS c ON [c].[Id] = [o].[CustomerId]
+	INNER JOIN [Person].[Person] AS p ON [p].[Id] = [c].[PersonId]
+	WHERE ot.[Name]=@Type
+
+RETURN 0
